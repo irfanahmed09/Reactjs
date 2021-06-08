f2be127a612edfd9750b9dfa9d0704cada6fe2ed
Irfan Ahmed <irfanahmed0915@gmail.com>
2021-06-08 03:54:45 -0500

    Counter app

---
+++ counter-app/README.md
@@ -0,0 +1,20 @@
+# Reactjs
+Learning React js 
+
+# A simple counter-App using React.js
+This tiny application is like adding,removing,deleting, resetting products which reflects in the total number of products into our shopping cart.
+
+![image](https://user-images.githubusercontent.com/43611717/121155000-08a39380-c80d-11eb-9e7b-46a3be565fb5.png)
+
+
+To run in your local machine
+
+**Scripts**  
+Install node modules
+
+`npm install`   
+In the project directory, you can run:
+
+`npm start`  
+Runs the app in the development mode.  
+Open http://localhost:3000 to view it in the browser.
---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "m-2 badge bg-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const count = this.props.counter.value;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
